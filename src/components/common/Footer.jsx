db315ef9b007d98cae8aabf7ac101285922fdd4c
sayad-mallllek <bob@khalilelzein.com>
2023-09-02 23:54:40 +0300

    init changes

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,29 +1,27 @@
-import * as Route from '@/constants/routes';
-import logo from '@/images/logo-full.png';
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import * as Route from "@/constants/routes";
+import logo from "@/images/logo.png";
+import bytetactix from "@/images/bytetactix-logo.png";
+import React from "react";
+import { useLocation } from "react-router-dom";
 
 const Footer = () => {
   const { pathname } = useLocation();
 
-  const visibleOnlyPath = [
-    Route.HOME,
-    Route.SHOP
-  ];
+  const visibleOnlyPath = [Route.HOME, Route.SHOP];
 
   return !visibleOnlyPath.includes(pathname) ? null : (
     <footer className="footer">
       <div className="footer-col-1">
         <strong>
           <span>
-            Developed by
-            {' '}
-            <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
+            Developed by{" "}
+            {/* <a href="https://github.com/jgudo">JULIUS GUEVARRA</a> */}
+            <img src={bytetactix} height={30} />
           </span>
         </strong>
       </div>
       <div className="footer-col-2">
-        <img alt="Footer logo" className="footer-logo" src={logo} />
+        <img alt="Footer logo" className="footer-logo" src={logo} height={30} />
         <h5>
           &copy;&nbsp;
           {new Date().getFullYear()}
@@ -32,8 +30,8 @@ const Footer = () => {
       <div className="footer-col-3">
         <strong>
           <span>
-            Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
+            All rights reserved
+            {/* <a href="https://github.com/jgudo/ecommerce-react">HERE</a> */}
           </span>
         </strong>
       </div>
